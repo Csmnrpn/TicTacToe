76cc78f2b8fddc34dd0332557794453aa6c40ef1
Cosmin R <cosminripan33@gmail.com>
2021-10-22 20:23:38 +0300

    working on logic

--- script.js
+++ script.js
@@ -9,12 +9,11 @@
 const gameboardContainer = document.querySelector('.gameboardContainer');
 
 const Gameboard = (() => {
-    const gameboard = ['X','O','X','O','O','X','X','O','X'];
+    const gameboard = ['','','','','','','','','']
     const createBoard = (gameboard) => {
         for (let i=0; i< gameboard.length; i++){
             let div = document.createElement('div');
-            div.textContent = `${gameboard[i]}`;
-            div.classList.add('gridSquare');
+            div.classList.add('gridSquare',`${i}`);
             gameboardContainer.appendChild(div);
         }
     }
@@ -26,7 +25,27 @@ const Gameflow = (() => {
 })();
 
 const Player = () => {
-
+    const sign = 'X';
+    return {sign};
 }
 
+
 Gameboard.createBoard(Gameboard.gameboard);
+const playerOne = Player();
+
+let gridSquares = gameboardContainer.querySelectorAll('.gameboardContainer > div');
+
+let arrayPosition = 0;
+gridSquares.forEach(square => {
+    square.addEventListener('click', function(){
+        square.textContent = playerOne.sign;
+        Gameboard.gameboard[arrayPosition] = playerOne.sign;
+        arrayPosition++;
+    })
+})
+
+const winningMoves = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8]
+]

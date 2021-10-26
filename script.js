0211d554cb617f52ed6582dc0010f3790bc399a8
Cosmin R <cosminripan33@gmail.com>
2021-10-26 22:24:25 +0300

    game is almost playable

--- script.js
+++ script.js
@@ -1,51 +1,106 @@
-//==================================================================================
-// 1. Store gameboard as an array inside of a Gameboard object
-// 2. Players will be objects
-// - Try to have as little global code as possible
-// - Try putting everything inside module or factory
+/*
+Game contains:
 
-//==================================================================================
-// Game Logic
+- 1 Gameboard (module)
+- 2 or more Players (objects)
+- 1 Object that has the logic of the game
+
+Thing in the game:
+- Ability of players to mark board
+- Function that checks for win condition
+
+*/
+
 const gameboardContainer = document.querySelector('.gameboardContainer');
 
 const Gameboard = (() => {
-    const gameboard = ['','','','','','','','','']
-    const createBoard = (gameboard) => {
-        for (let i=0; i< gameboard.length; i++){
-            let div = document.createElement('div');
-            div.classList.add('gridSquare',`${i}`);
-            gameboardContainer.appendChild(div);
+    const gameboard = new Array(9);
+    const createBoard = () => {
+        for (let i = 0; i < gameboard.length; i++) {
+            let gridSquare = document.createElement('div');
+            gridSquare.classList.add('gridSquare');
+            gameboardContainer.appendChild(gridSquare);
         }
     }
     return {gameboard, createBoard};
 })();
 
-const Gameflow = (() => {
+Gameboard.createBoard();
+
+const Player = (name,mark) => {
+    const placeMark = (index) => {
+            Gameboard.gameboard[index] = mark;        
+    }
+    return {placeMark, mark, name};
+}
+
+const jony = Player('Jonathan', 'X');
+const jeny = Player('Jenitina', 'O');
+
+
+const win = ((currentPlayer) => {
+    let winCounter = 0;
+    const winCondition = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
+    const winMessage = document.querySelector('.win_message');
+
+    const checkWin = (currentPlayer) => {    
+        winCondition.forEach(condition => {
+            condition.forEach(index => {
+                if(Gameboard.gameboard[index] === currentPlayer.mark) {
+                    winCounter ++;
+                }
+            })
+            if(winCounter === 3) {
+                winCounter = 0;
+                winMessage.textContent = `${currentPlayer.name} has won the game`;
+            }
+            else {
+                winCounter = 0;
+            }
+        })
+        console.log(`function ran for ${currentPlayer.name}`);
+    }
+    return {checkWin};
 
 })();
 
-const Player = () => {
-    const sign = 'X';
-    return {sign};
-}
+const gamelogic = (() => {
+    let currentPlayer = jony;
+    let nextPlayer = jeny;
+    let playerSwap = '';
+    let turnCounter = 0;
 
+    const isLegalMove = (index) => {
+        if (Gameboard.gameboard[index] !== undefined) {
+            return false;
+        }
+        else return true;
+    }
 
-Gameboard.createBoard(Gameboard.gameboard);
-const playerOne = Player();
+    const gridSquares = document.querySelectorAll('.gameboardContainer > div');
+    gridSquares.forEach((square, index) => {
+        square.addEventListener('click', () => {
+            if (isLegalMove(index)) {
+                square.textContent = currentPlayer.mark;
+                currentPlayer.placeMark(index);
 
-let gridSquares = gameboardContainer.querySelectorAll('.gameboardContainer > div');
-
-let arrayPosition = 0;
-gridSquares.forEach(square => {
-    square.addEventListener('click', function(){
-        square.textContent = playerOne.sign;
-        Gameboard.gameboard[arrayPosition] = playerOne.sign;
-        arrayPosition++;
-    })
-})
-
-const winningMoves = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8]
-]
+                turnCounter++;
+                if (turnCounter >= 5) {
+                    win.checkWin(currentPlayer);
+                }
+                
+                playerSwap = currentPlayer;
+                currentPlayer = nextPlayer;
+                nextPlayer = playerSwap;
+            }
+            else {
+                console.log('Move is not legal');
+            }
+        })
+    }) 
+    return {currentPlayer};
+})();

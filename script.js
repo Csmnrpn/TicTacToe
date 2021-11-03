a15fd249c366eb91acea486b1ebc6ca72ae199dd
Cosmin R <cosminripan33@gmail.com>
2021-11-03 18:50:11 +0200

    refactored code to make it more readable

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//=================================================================================
+// Creates gameboard with 9 squares
+
 const Gameboard = (() => {
     const gameboard = new Array(9);
     const gameboardContainer = document.querySelector('.gameboardContainer');
@@ -11,7 +14,8 @@ const Gameboard = (() => {
     return {gameboard, createBoard};
 })();
 
-Gameboard.createBoard();
+//=================================================================================
+// the player object
 
 const Player = (name,mark, color) => {
     const placeMark = (index) => {
@@ -20,8 +24,12 @@ const Player = (name,mark, color) => {
     return {placeMark, mark, name, color};
 }
 
+Gameboard.createBoard();
 const playerOne = Player('JohnDoe', 'X', '#e63946');
 const playerTwo = Player('JaneDoe', 'O', '#1d3557');
+
+//=================================================================================
+// win verification module
 
 const win = (() => {
     let winCounter = 0;
@@ -31,8 +39,6 @@ const win = (() => {
         [0,4,8], [2,4,6]
     ]
     
-    
-
     const checkWin = (currentPlayer) => { 
         let _workaround = false;   
         winCondition.forEach(condition => {
@@ -49,22 +55,25 @@ const win = (() => {
                 winCounter = 0;
             }
         })
-        console.log(`function ran for ${currentPlayer.name}`);
         return _workaround;
     }
     return {checkWin};
 
 })();
 
+//=================================================================================
+// game play logic
+
 const gamelogic = (() => {
     const winMessage = document.querySelector('.win_message');
     const gridSquares = document.querySelectorAll('.gameboardContainer > div');
+    const resetButton = document.querySelector('.resetButton');
+
     let currentPlayer = playerOne;
     let _nextPlayer = playerTwo;
     let _playerSwap = '';
     let _turnCounter = 0;
-
-    const resetButton = document.querySelector('.resetButton');
+    
     const resetGame = () => {
         Gameboard.gameboard = new Array(9);
         gridSquares.forEach((square) => {
@@ -78,9 +87,9 @@ const gamelogic = (() => {
         winMessage.textContent = '';
     }
 
-    resetButton.addEventListener('click', resetGame);
+    resetButton.addEventListener('click', resetGame);    
     
-    
+    // listens for click events to place markers
     const gameStart = () => {
         gridSquares.forEach((square, index) => {
             square.addEventListener('click', () => {
@@ -89,6 +98,7 @@ const gamelogic = (() => {
                     currentPlayer.placeMark(index);
 
                     _turnCounter++;
+                    // this runs the win function on each valid click event
                     if (win.checkWin(currentPlayer)) {
                         console.log('GAME WAS WON. GAME IS OVER');
                         winMessage.textContent = `${currentPlayer.name} has won the game.`;
@@ -112,8 +122,7 @@ const gamelogic = (() => {
     return {currentPlayer, gameStart, resetGame, resetButton};
 })();
 
-gamelogic.gameStart();
-
+// deals mostly with the first page, name generation for players and game start
 const mainContent = (() => {
     const playerNameOne = document.querySelector('.playerNameOne');
     const playerNameTwo = document.querySelector('.playerNameTwo');
@@ -123,6 +132,7 @@ const mainContent = (() => {
     const nameTwo = document.querySelector('#nameTwo');
 
     startButton.addEventListener('click', () => {
+        gamelogic.gameStart();
         startScreen.style.display = 'none';
         if (nameOne.value !== '') {
         playerOne.name = nameOne.value;
